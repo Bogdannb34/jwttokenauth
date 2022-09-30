8fbb583d103846c2b5a2f679b9a6cfab0becc28e
Bogdannb34 <bogdan22bogdan@gmail.com>
2022-09-30 15:10:14 +0300

    initial

---
+++ src/main/java/com/practice/jwttokenauth/JwttokenauthApplication.java
@@ -0,0 +1,13 @@
+package com.practice.jwttokenauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwttokenauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwttokenauthApplication.class, args);
+	}
+
+}
